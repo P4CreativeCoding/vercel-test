aa46be16ee9bd17cba0b0b8ba9e76a62ac578f1c
Max Studanski <mstu@posteo.net>
2023-05-31 16:28:30 +0200

    Replace test message

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('test');
+  res.send(`ey ihr könnt einfach mit dem vercel cli deployen. 
+   [0. cd /path/to/your/repo]
+    1. npm i -g vercel 
+    2. vercel login 
+    3. vercel --prod
+    Is dann nur nich offiziell mit Github verknüpft aber so what 🤷  `);
 });
 
 app.listen(port, () => {
