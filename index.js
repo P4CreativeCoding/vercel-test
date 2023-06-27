e4502eb661dd167b37075ae47e45c03c13d00ff1
franzidu <stfrdudi@stud.h-da.de>
2023-06-27 10:26:13 +0200

    add server

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`ey ihr könnt einfach mit dem vercel cli deployen. 
-   [0. cd /path/to/your/repo]
-    1. npm i -g vercel 
-    2. vercel login 
-    3. vercel --prod
-    Is dann nur nich offiziell mit Github verknüpft aber so what 🤷  `);
+  res.send('test');
 });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
